e49844696a30713661940ae17f71460faea36a47
tgh-joalbert <joalbertgonzalez@gmail.com>
2016-07-07 07:39:17 -0400

    front-end

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -4,7 +4,7 @@ class RolesController < ApplicationController
   # GET /roles
   # GET /roles.json
   def index
-    @roles = Role.all
+    @roles = Role.all.page(params[:page]).per(15)
   end
 
   # GET /roles/1
