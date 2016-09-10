3b6ad0885aab4b8de3bacd84ea046a2abc42842f
tgh-joalbert <joalbertgonzalez@gmail.com>
2016-09-10 12:46:53 -0400

    para generar pdf desde heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -22,12 +22,16 @@
 
 platform = RUBY_PLATFORM
 
-if platform.include?("darwin") # OS X machine
-  binary_path = Rails.root.join('bin', 'wkhtmltopdf-0.9.9-OS-X-i386').to_s
-elsif platform.include?("64-linux") # 64-bit linux machine
-  binary_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
-elsif platform.include?("x64-mingw")
-  binary_path = 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
+if Rails.env.production?
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+  WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
+else
+  if platform.include?("darwin") # OS X machine
+    binary_path = Rails.root.join('bin', 'wkhtmltopdf-0.9.9-OS-X-i386').to_s
+  elsif platform.include?("64-linux") # 64-bit linux machine
+    binary_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+  elsif platform.include?("x64-mingw")
+    binary_path = 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
+  end
+  WickedPdf.config = { :exe_path => binary_path }
 end
-
-WickedPdf.config = { :exe_path => binary_path }
