e65eec15bda0602c5ed00c5c1c00dce3aa7e1b7e
tgh-joalbert <joalbertgonzalez@gmail.com>
2016-10-01 23:46:50 -0400

    add mailer to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mailer
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => ENV["DOMAIN"] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: ENV["SMTP_SERVER"],
+      openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
+      port: ENV["SMTP_PORT"].to_i,
+      domain: ENV["MAILER_DOMAIN"],
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["SMTP_USER"],
+      password: ENV["SMTP_PWD"]
+  }
 end
