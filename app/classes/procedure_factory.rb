3b02063ec8c159d4400cc0628c00bda74b7324a8
alejandro matute <alexghoro@outlook.com>
2017-04-20 10:01:33 -0400

    fix Typo

--- app/classes/procedure_factory.rb
+++ app/classes/procedure_factory.rb
@@ -2,7 +2,7 @@ class ProcedureFactory
   def build(code)
     case code
       when 'T-AS100'  then SabbaticalYear.new
-      when 'T-AS101'  then ModifyWorkPlan.new
+      when 'T-AS101'  then ModifyWorkplan.new
     end
   end 
 end
