119611c48864dbe8687ffaf34b33fff2b738dc53
Alejandro Matute <alexghoro@outlook.com>
2017-09-01 12:08:08 -0400

    Change the defaut locale to ES Fix redirect path after user creation

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,21 @@ module Sistramad
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :es
+    
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    # autoload lib path
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
+    #predefinir sass
+    config.sass.preferred_syntax = :sass
+
+
   end
 end
